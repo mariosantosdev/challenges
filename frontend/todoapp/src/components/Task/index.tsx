ae4784e7a4fdbc342437a38bd72bffb388e36dbb
nevr001 <playcar46@gmail.com>
2021-01-30 12:45:33 -0300

    Update Task Component
    Inser button to delete all tasks completed

--- frontend/todoapp/src/components/Task/index.tsx
+++ frontend/todoapp/src/components/Task/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { IconButton, Checkbox, Container, FormControlLabel } from '@material-ui/core'
+import { IconButton, Checkbox, Container, FormControlLabel, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@material-ui/core'
 import { Delete } from '@material-ui/icons'
 
 import { TabsType } from '../Slider'
@@ -12,14 +12,51 @@ export type TasksType = { id: string, text: string, checked: boolean }
 export interface IPropsTask {
     tasks: Array<TasksType>
     onChange: (id: string) => void
-    onDelete: (id: string) => void
+    onDelete: (props: { id?: string, clean?: boolean }) => void
     currentTabShow: TabsType
 }
 
 const Task: React.FC<IPropsTask> = (props) => {
+    const [showDialog, setShowDialog] = React.useState(false)
+
+    const handleShowDialog = () => setShowDialog(!showDialog)
+
+    const completedTasks = props.tasks.filter(item => item.checked).length
+
+    const AlertDelete = () => {
+        const confirmDeleteTasks = () => {
+            props.onDelete({ clean: true })
+            setShowDialog(false)
+        }
+
+        return (
+            <Dialog
+                open={showDialog}
+                onClose={handleShowDialog}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">Confirm delete all tasks</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        You has sure delete ALL completed tasks ?
+                </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleShowDialog} color="default">
+                        Cancel
+                </Button>
+                    <Button onClick={confirmDeleteTasks} color="secondary" autoFocus>
+                        Confirm
+                </Button>
+                </DialogActions>
+            </Dialog>
+        )
+    }
 
     return (
         <Container className='container-task'>
+            <AlertDelete />
             {props.tasks.map(item => (
                 <div className='task-item' key={item.id}>
                     <FormControlLabel
@@ -27,12 +64,19 @@ const Task: React.FC<IPropsTask> = (props) => {
                         label={item.text}
                     />
                     {props.currentTabShow === 'completed' && (
-                        <IconButton onClick={() => props.onDelete(item.id)} aria-label="delete" color="secondary">
+                        <IconButton onClick={() => props.onDelete({ id: item.id })} aria-label="delete" color="secondary">
                             <Delete />
                         </IconButton>
                     )}
                 </div>
             ))}
+            {(props.currentTabShow === 'completed' && completedTasks > 0) && (
+                <div className='footer-tasks'>
+                    <Button onClick={handleShowDialog} color="secondary">
+                        <Delete /> Delete all
+                    </Button>
+                </div>
+            )}
         </Container>
     )
 }
